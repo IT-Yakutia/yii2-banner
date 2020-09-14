9535350dd8ba9331fe6b884607867a0bee18e2f2
playerzero <general.dmf@gmail.com>
2020-09-14 07:59:26 +0300

    namespace fixed

--- src/tests/fixtures/BannerFixture.php
+++ src/tests/fixtures/BannerFixture.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace ityakutia\poll\tests\fixtures;
+namespace ityakutia\banner\tests\fixtures;
 
 use ityakutia\banner\models\Banner;
 use yii\test\ActiveFixture;
@@ -8,5 +8,5 @@ use yii\test\ActiveFixture;
 class BannerFixture extends ActiveFixture
 {
     public $modelClass = Banner::class;
-    public $dataFile = '@ityakutia/banner/tests/_data/poll.php';
+    public $dataFile = '@ityakutia/banner/tests/_data/banner.php';
 }
