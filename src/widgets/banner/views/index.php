8fadec1320af261716794d3ad3d6c182fd8f2d4b
Ayaal Kaplin <uraankhayayaal@gmail.com>
2022-04-22 00:10:59 +0900

    add less styles

--- src/widgets/banner/views/index.php
+++ src/widgets/banner/views/index.php
@@ -10,8 +10,8 @@ $assetBundle = BannerAsset::register($this);
 <section id="hero" class="owl-carousel">
     <?php foreach ($banners as $key => $banner) { ?>
     <div>
-        <img class="hero_background img-fluid banner-bg" src="<?= $banner->photo ?>" alt="<?= $banner->title ?>">
-        <div class="hero_logotype d-flex align-items-center banner-wrapper">
+        <img class="img-fluid" src="<?= $banner->photo ?>" alt="<?= $banner->title ?>">
+        <div class="banner-wrapper d-flex align-items-center">
             <div class="container">
                 <div class="row">
                     <div class="col d-flex flex-row">
@@ -21,10 +21,10 @@ $assetBundle = BannerAsset::register($this);
                         <div class="d-flex align-items-center">
                             <div>
                                 <?php if(!empty($banner->title)) { ?>
-                                    <h1 class="text-white ml-3 banner-item-title"><?= $banner->title ?></h1>
+                                    <h1 class="banner-item-title text-white ml-3 "><?= $banner->title ?></h1>
                                 <?php } ?>
                                 <?php if(!empty($banner->description)) { ?>
-                                    <p class="text-white ml-3 banner-item-description"><?= $banner->description ?></p>
+                                    <p class="banner-item-description text-white ml-3 "><?= $banner->description ?></p>
                                 <?php } ?>
                                 <?php if(!empty($banner->link)) { ?>
                                     <?= Html::a("Подробнее", $banner->link, ['class' => 'banner-item-link ml-3 btn text-white', 'target' => '_blank']) ?>
